5ee340f78b6e640d3991a93718572eee2e4bc583
Vaania Asif <vaania.asif@dubizzlelabs.com>
2023-11-06 10:27:50 +0500

    main screen

--- lib/screens/main_bottom_nav/main_home_screen.dart
+++ lib/screens/main_bottom_nav/main_home_screen.dart
@@ -6,6 +6,7 @@ import 'package:pak_saaf/utils/global.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../../common/custom_animation.dart';
 import '../../../string_en.dart';
+import '../../utils/view_constants.dart';
 import '../qr_scanner/qr_scan_screen.dart';
 
 class MainHomeScreen extends StatefulWidget {
@@ -55,11 +56,15 @@ class _MainHomeScreenState extends State<MainHomeScreen> {
                             ),
                             margin: EdgeInsets.all(16.0),
                             child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center, // Center align the children
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              // Center align the children
                               children: [
                                 Padding(
                                   padding: EdgeInsets.only(
-                                      left: 16.0, right: 16.0, top: 50.0, bottom: 50),
+                                      left: 16.0,
+                                      right: 16.0,
+                                      top: 50.0,
+                                      bottom: 50),
                                   child: Text(
                                     "$points pts",
                                     style: TextStyle(
@@ -83,8 +88,7 @@ class _MainHomeScreenState extends State<MainHomeScreen> {
                     ],
                   ),
 
-
-                  Row(
+                  /*Row(
                     children: [
                       const Text(
                         "OFFERS", // Text above the cards
@@ -95,8 +99,8 @@ class _MainHomeScreenState extends State<MainHomeScreen> {
                       ),
 
                     ],
-                  ),
-                  const Card(
+                  ),*/
+                  /* const Card(
                     child: ListTile(
                       leading: Icon(Icons.icecream), // Icon at the left corner
                       title: Text("Get A McDonalds McFlurry"),
@@ -117,20 +121,69 @@ class _MainHomeScreenState extends State<MainHomeScreen> {
                       title: Text("Get 50% off Your Next Yango Ride"),
                       subtitle: Text("180 pts required"),
                     ),
+                  ),*/
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+
+                      children: [
+                        Text(
+                          'Redeem Now',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w900,
+                          ),
+                        ),
+                        SizedBox(width: 16), // Add some spacing between the text and the TextField
+                        Container(
+                          width: 150, // Set the desired width for the TextField
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10.0), // Optional: Add rounded corners
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey, // Shadow color
+                                  offset: Offset(0, 3), // Offset of the shadow
+                                  blurRadius: 6, // Blur radius of the shadow
+                                ),
+                              ],
+                            ),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                hintText: "Enter points here",
+                                contentPadding: EdgeInsets.all(10.0), // Optional: Adjust content padding
+                                border: InputBorder.none, // Hide the default border
+                              ),
+                            ),
+                          ),
+                        )
+
+                      ],
+                    ),
                   ),
+
                   Align(
-                    alignment: Alignment.bottomRight,
+                    alignment: Alignment.center,
                     child: Padding(
                       padding: const EdgeInsets.all(16.0),
-                      child: FloatingActionButton(
-                        onPressed: () {
-                          Navigator.of(context)
-                              .push(bottomToTop(QRScanScreen()));
-                        },
-                        child: Icon(Icons.qr_code),
-                        // Replace with the icon you want
-                        backgroundColor: ThemeColor
-                            .secondaryColor, // Set the background color
+                      child: Container(
+                        width: 150,
+                        height: 150,
+                        margin: ViewConstants.scaledEdgeInsets(context: context, top: 40),
+                        child: FloatingActionButton(
+                          onPressed: () {
+                            Navigator.of(context)
+                                .push(bottomToTop(QRScanScreen()));
+                          },
+                          child:Transform.scale(
+                            scale: 3, // Adjust the scale factor to increase icon size
+                            child: Icon(Icons.qr_code),
+                          ),
+                          // Replace with the icon you want
+                          backgroundColor: ThemeColor.secondaryColor, // Set the background color
+                        ),
                       ),
                     ),
                   ),
